d8b9c2e81856ebfe64c9fa07c1f615f9fd614fcf
Olaf Alders <olaf@wundersolutions.com>
2020-01-14 16:50:52 -0500

    Allow networks to be passed without using flags

--- cmd/mmdbinspect/main.go
+++ cmd/mmdbinspect/main.go
@@ -20,23 +20,39 @@ func (i *arrayFlags) Set(value string) error {
 	return nil
 }
 
+func usage() {
+	fmt.Printf(
+		"Usage: %s --db path/to/db --db path/to/other/db 130.113.64.30/24 0:0:0:0:0:ffff:8064:a678\n",
+		os.Args[0],
+	)
+	flag.PrintDefaults()
+	fmt.Print(`
+Any additional arguments passed are assumed to be networks to look up.  If an
+address range is not supplied, /32 will be assumed for ipv4 addresses and /128
+will be assumed for ipv6 addresses.
+`)
+}
+
 func main() {
-	var mmdb, network arrayFlags
+	var mmdb arrayFlags
 
 	flag.Var(&mmdb, "db", "Path to an mmdb file. You may pass this arg more than once.")
-	flag.Var(&network, "network", "A network address to look up. You may pass this arg more than once.")
 
+	flag.Usage = usage
 	flag.Parse()
+
+	// Any remaining arguments (not passed via flags) should be networks
+	network := flag.Args()
 
 	if len(network) == 0 {
 		fmt.Println("You must provide at least one network address")
-		flag.PrintDefaults()
+		usage()
 		os.Exit(1)
 	}
 
 	if len(mmdb) == 0 {
 		fmt.Println("You must provide a path to at least one .mmdb file")
-		flag.PrintDefaults()
+		usage()
 		os.Exit(1)
 	}
 
